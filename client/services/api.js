ef16d4e449b2de4e8be5e87c45f50c305af6fe0c
Aman Jaiswal <amanjaiswal@Amans-MacBook.local>
2026-07-05 13:58:52 +0530

    chore: update API base URL to production render endpoint

--- client/services/api.js
+++ client/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chatai-s9ih.onrender.com/api",
 });
 
 const SESSION_KEY = "chat-session-id";
